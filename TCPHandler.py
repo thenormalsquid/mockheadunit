1ce9bc38bdc9d0d74571daed71383d4045b809ef
radicalcakes <tbhuynh10@gmail.com>
2015-01-08 15:19:42 -0500

    need to fix chksum

--- TCPHandler.py
+++ TCPHandler.py
@@ -3,7 +3,7 @@ __author__ = 'radicalcakes'
 #raw socket server for sending the tcp packet to a configurable ip address
 import socket
 import binascii
-import struct
+from struct import *
 from config import CONFIG
 
 
@@ -21,14 +21,15 @@ class Packet(object):
     def _checksum(self, msg):
         #creates checksum for the tcp header
         s = 0
-
-        # loop taking 1 character at a time
-        for i in range(0, len(msg)):
-            w = ord(msg[i])
+        print msg
+        # # loop taking 1 character at a time
+        for i in xrange(len(msg)):
+            w = int(msg[i], 16)
             s = s + w
 
-        s = (s>>16) + (s & 0xff);
-        s = s + (s >> 16);
+        print s
+        s = (s>>8) + (s & 0xff)
+        s = s + (s >> 8)
 
         #complement and mask to 4 byte short
         s = ~s & 0xff
@@ -38,9 +39,9 @@ class Packet(object):
     def _bool_to_int(self, val):
         #converts a bool value to representations of 1/0
         if val:
-            return 1
+            return hex(1)
         else:
-            return 0
+            return hex(0)
 
     def _parse_stat(self, data, stat):
         #ensure that the correct stat key is passed
@@ -48,13 +49,21 @@ class Packet(object):
             if stat == 'stat1':
                 stat1 = []
                 for key in STAT1KEYS:
-                    stat1.append(data[key])
-                return self._bool_to_int(stat1.count(True) > 1)
+                    for statobj in data:
+                        if key in statobj:
+                            stat1.append(statobj[key])
+                return self._bool_to_int(stat1.count(True) >= 1)
             else:
                 stat0 = []
                 for key in STAT0KEYS:
-                    stat0.append(data[key])
-                return self._bool_to_int(stat0.count(True) > 1)
+                    for statobj in data:
+                        if key in statobj:
+                            stat0.append(statobj[key])
+                        #this is for flipping the third bit
+                    if True not in stat0:
+                        stat0.append(True)
+
+                    return self._bool_to_int(stat0.count(True) >= 1)
         else:
             raise ValueError
 
@@ -64,16 +73,18 @@ class Packet(object):
         for key in DATAKEYS:
             #special case for the sec data
             if key == '0x3e':
-                initial_array.append(62)
+                initial_array.append(hex(62))
+            elif key not in data:
+                continue
             elif key == 'traceIds':
                 for trcid in data[key]:
-                    initial_array.append(trcid)
+                    initial_array.append(hex(trcid))
             elif key == 'stat1':
-                initial_array.append(self._parse_stat(data[key]))
+                initial_array.append(self._parse_stat(data[key], key))
             elif key == 'stat0':
-                initial_array.append(self._parse_stat(data[key]))
+                initial_array.append(self._parse_stat(data[key], key))
             else:
-                initial_array.append(data[key])
+                initial_array.append(hex(data[key]))
         return initial_array
 
     def _format_data(self, data):
@@ -83,25 +94,25 @@ class Packet(object):
         initial_arr = self._parse_data(data)
         packet_arr = []
         #first byte is 114
-        header = 114
+        header = hex(114)
         #add 1 to include the header info
-        length = len(initial_arr) + 1
+        length = hex(18)
 
         packet_arr.append(header)
         packet_arr.append(length)
+
         #add the other packet data ie; status, etc to the packet array
         packet_arr += initial_arr
-
         #compute chksum and append to the packet message
-        packet_tup = tuple(packet_arr)
-        packer = struct.Struct('I')
-        prepacked = packer.pack(*packet_tup)
-        #add in chksum
-        chksum = self._checksum(prepacked)
+        chksum = hex(self._checksum(packet_arr))
+
         packet_arr.append(chksum)
-        final_tup = tuple(packet_arr)
-        packed = packer.pack(*final_tup)
-        return packed
+        packed = b''.join(packet_arr)
+
+        msg = bytearray(packed)
+
+        print msg
+        return msg
 
     def _format_list(self, data):
         new_data = map(self._format_data, data)
--- tests.py
+++ tests.py
@@ -1,1 +1,53 @@
 __author__ = 'radicalcakes'
+import TCPHandler
+
+test_json_obj = {
+                #hidden server sould do the conversion of originators and firstHopid
+                "originator": 2995272300,
+                "firstHop": 25721492,
+                "traceCount": 00,
+                "hopCount": 01,
+                "pti": 00,
+                "stat1":[
+                    {
+                        "tamper": False
+                    },
+                    {
+                        "cleanMe": False
+                    },
+                    {
+                        "alarm4": False
+                    },
+                    {
+                        "alarm3": False
+                    },
+                    {
+                        "alarm2": False
+                    },
+                    {
+                        "primaryAlarm": False
+                    }
+
+                ],
+                "stat0": [
+                    {
+                        "reserved": False
+                    },
+                    {
+                        "lowBattery": False
+                    },
+                    {
+                        "caseTamper": False
+                    },
+                    {
+                        "reset": False
+                    }
+                ],
+                "level": 00,
+                "margin": 00
+            }
+
+#firsthopid: 01 88 7A 94
+#originator: B2 88 3A 6C
+
+packet = TCPHandler.Packet(test_json_obj)
